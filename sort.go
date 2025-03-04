337f915af704a1f8a5cb537bb5b1da4fba2f77dd
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2025-03-04 11:30:51 +0700

    sort

---
+++ sort.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+type UserSlice []User
+
+func (userSlice UserSlice) Len() int {
+	return len(userSlice)
+}
+
+func (userSlice UserSlice) Less(i, j int) bool {
+	return userSlice[i].Age < userSlice[j].Age
+}
+
+func (userSlice UserSlice) Swap(i, j int) {
+	userSlice[i], userSlice[j] = userSlice[j], userSlice[i]
+}
+
+func main() {
+	users := []User{
+		{"Syafiq", 30},
+		{"Fajrian", 35},
+		{"Emha", 25},
+		{"Budi", 23},
+	}
+
+	sort.Sort(UserSlice(users))
+
+	fmt.Println(users)
+}
